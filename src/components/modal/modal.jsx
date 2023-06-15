cf58c0bceda948d2b1df74a67cfbe81c531c12fe
DimaHohol <terokhin.d@gmail.com>
2023-06-15 22:40:39 +0300

    4.1

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,71 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import "../ProductCard/ProductCard.css";
-import Modal from "../modal/modal";
+import "./modal.css";
 
-const CartPage = ({ cartItems, favorites, removeFromCart }) => {
-  const [showModal, setShowModal] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(null);
-
-  const openModal = (item) => {
-    setSelectedItem(item);
-    setShowModal(true);
-  };
-
-  const closeModal = () => {
-    setShowModal(false);
-    setSelectedItem(null);
-  };
-
-  const handleRemoveFromCart = () => {
-    if (selectedItem) {
-      removeFromCart(selectedItem);
-      setSelectedItem(null);
-      setShowModal(false);
-    }
-  };
-
+const Modal = ({ header, closeButton, text, actions, onClose }) => {
   return (
-    <div>
-      <h2>Cart Items:</h2>
-      <ul>
-        {cartItems.map((item, index) => (
-          <div className="product-card" key={index}>
-            {favorites.includes(item.article) && (
-              <span className="favorite-icon active">
-                &#9734; товар в списку улюблених
-              </span>
-            )}
-            <img className="product-image" src={item.img_url} alt={item.name} />
-            <h3 className="product-name">{item.name}</h3>
-            <p className="product-price">${item.price}</p>
-            <p className="product-article">Article: {item.article}</p>
-            <button onClick={() => openModal(item)}>Remove from Cart</button>
-          </div>
-        ))}
-      </ul>
-      {showModal && (
-        <Modal
-          header="Remove from Cart"
-          closeButton={true}
-          text="Are you sure you want to remove this item from your cart?"
-          actions={
-            <>
-              <button onClick={handleRemoveFromCart}>Confirm</button>
-              <button onClick={closeModal}>Cancel</button>
-            </>
-          }
-          onClose={closeModal}
-        />
-      )}
+    <div className="modal">
+      <div className="modal-content">
+        {closeButton && (
+          <button className="close-button" onClick={onClose}>
+            X
+          </button>
+        )}
+        <h2 className="modal-header">{header}</h2>
+        <div className="modal-text">{text}</div>
+        <div className="modal-actions">{actions}</div>
+      </div>
     </div>
   );
 };
 
-CartPage.propTypes = {
-  cartItems: PropTypes.array.isRequired,
-  favorites: PropTypes.array.isRequired,
-  removeFromCart: PropTypes.func.isRequired,
+Modal.propTypes = {
+  header: PropTypes.string.isRequired,
+  closeButton: PropTypes.bool,
+  text: PropTypes.string.isRequired,
+  actions: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
-export default CartPage;
+Modal.defaultProps = {
+  closeButton: false,
+};
+
+export default Modal;
